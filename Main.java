795cb7b5ba8e82e4abf34839395e59c1a86996a6
NagihanKURNAZ <112775522+NagihanKURNAZ@users.noreply.github.com>
2022-12-29 19:39:18 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,43 +1,63 @@
+import java.nio.channels.SelectableChannel;
 import java.util.Scanner;
+
+import org.w3c.dom.ls.LSOutput;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		double n1, n2;
-		int select;
-
+		
+		String userName = "patika";
+		String password = "Java123";
+		String select;
+		String newPassword;
+		boolean check = true;
+		
 		Scanner input = new Scanner(System.in);
-
-		System.out.print("İlk sayıyı giriniz: ");
-		n1 = input.nextInt();
-		System.out.print("İkinci sayıyı giriniz: ");
-		n2 = input.nextInt();
-
-		System.out.println("1-Toplama\n 2-Çıkarma\n 3-Çarpma\n 4-Bölme");
-		System.out.println("Seçiminiz: ");
-		select = input.nextInt();
-
-		switch (select) {
-		case 1:
-			System.out.println("Toplama işleminin sonucu:" + (n1 + n2));
-			break;
-		case 2:
-			System.out.println("Çıkarma işleminin sonucu:" + (n1 - n2));
-			break;
-		case 3:
-			System.out.println("Çarpma işleminin sonucu:" + (n1 * n2));
-			break;
-		case 4:
-			if (n2 == 0) {
-				System.out.println("! Math Error");
-			} else {
-				System.out.println("result : " + (n1 / n2));
-			}
-			break;
-		default:
-			System.out.println("Yanlış bir seçim yaptınız");
-			break;
+		
+		System.out.println("Kullanıcı  Adınızı Giriniz: ");
+		userName=input.nextLine();
+		System.out.println("Şifrenizi Giriniz: ");
+		password=input.nextLine();
+		
+		if(userName.equals("patika")&&password.equals("Java123")){
+			System.out.println("giriş yaptınız.");
 		}
+			else if(userName!="patika") {
+				System.out.println("Kullanıcı adınızı yanlış girdiniz.");	
+			}
+			else if(password!="Java123") {
+				System.out.println("şifrenizi yanlış girdiniz.");
+				
+			}
+		while (check){
+            System.out.print("Şifrenizi değiştirmek ister misiniz? (y/n)"); int a = input.nextLine().charAt(0);
+            switch (a){
+                case 'y' :
+                    System.out.print("Lütfen yeni şifrenizi giriniz:"); newPassword = input.nextLine();
+                    if (newPassword.equals("Java123") || newPassword.equals(password)){
+                        System.out.println("Şifreniz eski şifreniz ile aynı olamaz. Üst menüye aktarılıyorsunuz.");
+                    }
+                    else {
+                        System.out.println("Yeni şifreniz başarılı bir şekilde oluşturuldu.");
+                        System.out.println("Çıkış yapılıyor...");
+                        check = false;
+                        break;
+                    }
+                    break;
+                case 'n':
+                    System.out.println("Çıkış yapılıyor..");
+                    check = false;
+                    break;
+                default:
+                    System.out.println("Hatalı işlem seçtiniz. Üst menüye aktarılıyorsunuz.");
+		
+		}
+		
 	}
+		
+		
+		
+	}
+
 }
